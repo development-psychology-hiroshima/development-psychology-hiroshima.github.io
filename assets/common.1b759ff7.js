b80ac1dcbc91e0e037978e4f10434696bee1903b
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-05-04 23:13:29 +0900

    bugfix

---
+++ assets/common.1b759ff7.js
@@ -0,0 +1,1 @@
+import{g as n,f as o,h as i,m as a,i as r}from"./getConfig.1a4f2903.js";const e=await n();o(i).provide("config",e).mount("#vue-carousel");o(a).provide("config",e).mount("#mobile-menu");o(r).provide("config",e).mount("#container-desktop-menu");
--- assets/common.4b9ba9b1.js
+++
@@ -1,1 +0,0 @@
-import{g as n,f as o,h as i,m as a,i as r}from"./getConfig.82bd85fe.js";const e=await n();o(i).provide("config",e).mount("#vue-carousel");o(a).provide("config",e).mount("#mobile-menu");o(r).provide("config",e).mount("#container-desktop-menu");
