c4a922702e22e65adf7cd505665dd7cfb6b86612
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-05-11 17:06:26 +0900

    [feature] polyfill

---
+++ assets/research_activity_sugimura-legacy.b03aa2d5.js
@@ -0,0 +1,1 @@
+System.register(["./getConfig-legacy.d81b2bf1.js","./common-legacy.eefd5630.js"],(function(){"use strict";return{setters:[function(){},function(){}],execute:function(){}}}));
---
+++ assets/research_activity_sugimura.05ef662b.js
@@ -0,0 +1,1 @@
+import"./getConfig.00ee822e.js";import{__tla as _}from"./common.1914d92d.js";let a,r=Promise.all([(()=>{try{return _}catch{}})()]).then(async()=>{a=function(){import("data:text/javascript,").then(async t=>(await t.__tla,t))}});export{r as __tla,a as __vite_legacy_guard};
