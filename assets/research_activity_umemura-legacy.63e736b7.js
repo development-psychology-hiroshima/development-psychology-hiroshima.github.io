d10d6380d5ebeca7aa6d8d51ae1e25565c56cb8e
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-05-13 16:03:24 +0900

    ux improve

---
+++ assets/research_activity_umemura-legacy.63e736b7.js
@@ -0,0 +1,1 @@
+System.register(["./getConfig-legacy.d043cd0d.js","./common-legacy.04a0dbf8.js"],(function(){"use strict";return{setters:[function(){},function(){}],execute:function(){}}}));
--- assets/research_activity_umemura-legacy.db828b0f.js
+++
@@ -1,1 +0,0 @@
-System.register(["./getConfig-legacy.d81b2bf1.js","./common-legacy.eefd5630.js"],(function(){"use strict";return{setters:[function(){},function(){}],execute:function(){}}}));
