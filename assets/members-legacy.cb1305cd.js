7f71f172e34b854c73a95319fd1b04b56f1e28f2
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-09-17 00:18:38 +0900

    nightly build

---
+++ assets/members-legacy.cb1305cd.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy.f5fd108c.js"],(function(e,t){"use strict";return{setters:[function(){}],execute:function(){}}}));
--- assets/umemura-legacy.f4b9785f.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy.e9d5d61d.js"],(function(e,t){"use strict";return{setters:[function(){}],execute:function(){}}}));
