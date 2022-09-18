d2e8c51548a89484436915e6dcc44fc1a10354c3
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-09-19 01:10:18 +0900

    nightly

--- assets/sugimura-legacy.1b0e77fd.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy.f398dd88.js"],(function(e,t){"use strict";return{setters:[function(){}],execute:function(){}}}));
---
+++ assets/umemura-legacy.3abd0e21.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy.fc536768.js"],(function(e,t){"use strict";return{setters:[function(){}],execute:function(){}}}));
