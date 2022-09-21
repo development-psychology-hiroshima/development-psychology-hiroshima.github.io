e3e84a003525bd5a761ac52fa7ff5d2ded2ba18d
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-09-21 11:47:47 +0900

    test new cache logic

---
+++ assets/index.6909b8c0.js
@@ -0,0 +1,1 @@
+export function __vite_legacy_guard(){import("data:text/javascript,")}import"./main.a2db10e6.js";
---
+++ assets/members-legacy.7fe5e8b3.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy.28f70f1e.js"],(function(e,t){"use strict";return{setters:[function(){}],execute:function(){}}}));
